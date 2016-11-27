0994b6b1ad5a5c0aef3c2013bba80dfd0b7f95bf
yanheven <yanheven@qq.com>
2016-11-28 00:39:45 +0800

    1127 update
    Change-Id: I9a29398d3bebaf0e68faf5c65d5810ad3fcdc3a3

--- nova/tests/unit/virt/azureapi/test_adapter.py
+++ nova/tests/unit/virt/azureapi/test_adapter.py
@@ -2,6 +2,7 @@ import mock
 import unittest
 
 from nova.virt.azureapi import adapter
+from nova import test
 
 USERNAME = 'AZUREUSER'
 PASSWORD = 'PASSWORD'
@@ -16,7 +17,27 @@ class FakeKey(object):
         value = KEY
     keys = [Value()]
 
-class AzureTestCase(unittest.TestCase):
+
+class AzureTestCase(test.NoDBTestCase):
+
+    def _start_driver_with_flags(self, expected_exception_type, startup_flags):
+        self.flags(group='azure', **startup_flags)
+        self.assertRaises(Exception, adapter.Azure, None)  # noqa
+
+    def test_start_driver_no_username(self):
+        self._start_driver_with_flags(
+                Exception,
+                dict(subscription_id=SUBSCRIBE_ID, password=PASSWORD))
+
+    def test_start_driver_no_subscribe_id(self):
+        self._start_driver_with_flags(
+                Exception,
+                dict(username=USERNAME, password=PASSWORD))
+
+    def test_start_driver_no_password(self):
+        self._start_driver_with_flags(
+                Exception,
+                dict(username=USERNAME, subscription_id=SUBSCRIBE_ID))
 
     @mock.patch('nova.virt.azureapi.adapter.UserPassCredentials')
     @mock.patch('nova.virt.azureapi.adapter.ResourceManagementClient')
@@ -24,23 +45,22 @@ class AzureTestCase(unittest.TestCase):
     @mock.patch('nova.virt.azureapi.adapter.NetworkManagementClient')
     @mock.patch('nova.virt.azureapi.adapter.StorageManagementClient')
     @mock.patch('nova.virt.azureapi.adapter.CloudStorageAccount')
-    # @mock.patch.object(adapter.StorageManagementClient,
-    #                    'storage_accounts',
-    #                    return_value=mock.MagicMock)
-    # @mock.patch.object(adapter.CloudStorageAccount,
-    #                    'create_page_blob_service')
-    def test_init(self, cloudstorage, storage, netowrk, compute, resource,
-                  credential):
+    def test_start_driver_with_user_password_subscribe_id(
+            self, cloudstorage, storage, network,
+            compute, resource, credential):
+        self.flags(group='azure', username=USERNAME,
+                   password=PASSWORD, subscription_id=SUBSCRIBE_ID,
+                   storage_account=SAC)
         storage().storage_accounts.list_keys = mock.Mock(return_value=FakeKey)
         print storage.storage_accounts.list_keys()
         cloudstorage.create_page_blob_service = mock.Mock()
 
-        azure = adapter.Azure(USERNAME, PASSWORD, SUBSCRIBE_ID, RG, SAC)
+        azure = adapter.Azure()
 
         credential.assert_called_once_with(USERNAME, PASSWORD)
         resource.assert_called_once_with(credential(), SUBSCRIBE_ID)
         compute.assert_called_once_with(credential(), SUBSCRIBE_ID)
-        netowrk.assert_called_once_with(credential(), SUBSCRIBE_ID)
+        network.assert_called_once_with(credential(), SUBSCRIBE_ID)
         storage.assert_called_with(credential(), SUBSCRIBE_ID)
         cloudstorage.assert_called_once_with(account_name=SAC, account_key=KEY)
         self.assertTrue(hasattr(azure, 'blob'))
@@ -48,7 +68,3 @@ class AzureTestCase(unittest.TestCase):
         self.assertTrue(hasattr(azure, 'compute'))
         self.assertTrue(hasattr(azure, 'network'))
         self.assertTrue(hasattr(azure, 'storage'))
-
-
-if __name__ == '__main__':
-    unittest.main()
